bd8e459527dd353608c96ba8133fe73df4131f3b
Tayomide <tibrahim@hawk.iit.edu>
2023-01-27 20:14:47 -0600

    Add block scene to app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Block } from './pages/Block';
 import { Home } from './pages/Home';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route exact path="/block" element={<Block />} />
       </Routes>
     </Router>
   );
