71f9434103e5d06f2b5da930bb2d40c3700d1085
Tayomide <tibrahim@hawk.iit.edu>
2023-01-30 15:54:57 -0600

    Create empty component

---
+++ src/pages/LeetCode.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const LeetCode = () => {
+  // Multiply by a 1000 to get actual time
+  // timezone from leetcode * 1000 + 21600000
+  return (
+    <div>LeetCode</div>
+  )
+}
