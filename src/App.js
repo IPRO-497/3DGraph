22d9c4bb4e876c8b4ade431984d96a4e709123e7
Tayomide <tibrahim@hawk.iit.edu>
2023-01-30 15:55:58 -0600

    Test endpoints and add blank page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Block } from './pages/Block';
 import { Home } from './pages/Home';
+import { LeetCode } from './pages/LeetCode';
 
 function App() {
+  // fetch("http://localhost:5000/leetcode?username=tayomide", {
+  //   method: "GET"
+  // })
+  // .then(response => response.json())
+  // .then(response => console.log(response))
+
+  // fetch("http://localhost:5000/github?user=tayomide&year=2022", {
+  //   method: "GET"
+  // })
+  // .then(response => response.json())
+  // .then(response => console.log(response))
+
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/block" element={<Block />} />
+        <Route exact path="/leetcode" element={<LeetCode />} />
       </Routes>
     </Router>
   );
