a248611f0b140d238e803aea51ec8d53b329a05c
Tayomide <tibrahim@hawk.iit.edu>
2023-01-30 15:55:40 -0600

    Add dummy data for doulingo

---
+++ src/data/DoulingoDummy.js
@@ -0,0 +1,81 @@
+export const data = {
+  "summaries": [
+      {
+          "gainedXp": 15,
+          "frozen": false,
+          "streakExtended": true,
+          "date": 1674950400,
+          "userId": 52337,
+          "repaired": false,
+          "dailyGoalXp": 10,
+          "numSessions": 1,
+          "totalSessionTime": 62
+      },
+      {
+          "gainedXp": 15,
+          "frozen": false,
+          "streakExtended": true,
+          "date": 1674864000,
+          "userId": 52337,
+          "repaired": false,
+          "dailyGoalXp": 10,
+          "numSessions": 1,
+          "totalSessionTime": 82
+      },
+      {
+          "gainedXp": 15,
+          "frozen": false,
+          "streakExtended": true,
+          "date": 1674777600,
+          "userId": 52337,
+          "repaired": false,
+          "dailyGoalXp": 10,
+          "numSessions": 1,
+          "totalSessionTime": 84
+      },
+      {
+          "gainedXp": 15,
+          "frozen": false,
+          "streakExtended": true,
+          "date": 1674691200,
+          "userId": 52337,
+          "repaired": false,
+          "dailyGoalXp": 10,
+          "numSessions": 1,
+          "totalSessionTime": 123
+      },
+      {
+          "gainedXp": 5,
+          "frozen": false,
+          "streakExtended": true,
+          "date": 1674604800,
+          "userId": 52337,
+          "repaired": false,
+          "dailyGoalXp": 10,
+          "numSessions": 1,
+          "totalSessionTime": 78
+      },
+      {
+          "gainedXp": 5,
+          "frozen": false,
+          "streakExtended": true,
+          "date": 1674518400,
+          "userId": 52337,
+          "repaired": false,
+          "dailyGoalXp": 10,
+          "numSessions": 1,
+          "totalSessionTime": 74
+      },
+      {
+          "gainedXp": 5,
+          "frozen": false,
+          "streakExtended": true,
+          "date": 1674432000,
+          "userId": 52337,
+          "repaired": false,
+          "dailyGoalXp": 10,
+          "numSessions": 1,
+          "totalSessionTime": 86
+      }
+  ]
+}
