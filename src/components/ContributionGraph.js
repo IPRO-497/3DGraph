8ef56aec9b7166c2366243abaed10905b3a2ae7d
Tayomide <tibrahim@hawk.iit.edu>
2023-02-02 00:41:13 -0600

    Update code logic
    Substituting the double ampersand fixed a bug; returning null fixed the map must have a return statement bug.

--- src/components/ContributionGraph.js
+++ src/components/ContributionGraph.js
@@ -3,12 +3,14 @@ export const ContributionGraph = ({ data }) => {
     data.map(
       (week, key) => 
       <group position={[key / 2, 0, 0]} key={key}>
-        {week.map((day, dayKey) => day &&
-          <mesh position={[0, day / 20, dayKey / 2]} key={dayKey}>
-            <boxGeometry args={[0.5, day/10, 0.5]}/>
-            <meshBasicMaterial color="black" />
-          </mesh>
-        )}
+        {week.map((day, dayKey) => {
+          if(day){
+            return <mesh position={[0, day / 20, dayKey / 2]} key={dayKey}>
+              <boxGeometry args={[0.5, day/10, 0.5]}/>
+              <meshBasicMaterial color="black" />
+            </mesh>
+          }else return null
+        })}
       </group>
     )
   )
