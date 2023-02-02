795424ce23a40314cb3e6fc0032db4806f8e018c
Tayomide <tibrahim@hawk.iit.edu>
2023-02-02 00:15:50 -0600

    Create LeetCode Page

--- src/pages/LeetCode.js
+++ src/pages/LeetCode.js
@@ -1,8 +1,13 @@
+
 import React from 'react'
 
 export const LeetCode = () => {
   // Multiply by a 1000 to get actual time
   // timezone from leetcode * 1000 + 21600000
+  const leetCodeData = require("../data/LeetCodeDummy").data
+  const converter = require("../converter/LeetCode")
+  console.log(leetCodeData)
+  console.log(converter.convert(leetCodeData))
   return (
     <div>LeetCode</div>
   )
