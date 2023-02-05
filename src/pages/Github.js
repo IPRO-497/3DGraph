97c2379ecc5830694b09e2bd7900e3fdda617acd
Tayomide <tibrahim@hawk.iit.edu>
2023-02-04 19:50:33 -0600

    Add variables to request

--- src/pages/Github.js
+++ src/pages/Github.js
@@ -2,12 +2,15 @@ import { OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import { YearWeekDayGroup } from '../components/YearWeekDayGroup'
 import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 export const Github = () => {
+  // new Date().getFullYear() - 1
+  const params = useParams()
   const [convertedData, setConvertedData] = useState()
   useEffect(() => {
     const converter = require("../converter/Github")
-    fetch("/.netlify/functions/github")
+    fetch(`/.netlify/functions/github?name=${params.name}&year=${params.year}`)
     .then(response => response.json())
     .then(response => {
       setConvertedData(converter.convert(response))
@@ -16,7 +19,7 @@ export const Github = () => {
   return (
     <Canvas>
       <OrbitControls />
-      {convertedData && <YearWeekDayGroup convertedData={convertedData} username="tayomide" year="2022" website="github" />}
+      {convertedData && <YearWeekDayGroup convertedData={convertedData} username={params.name} year={params.year} website="github" />}
     </Canvas>
   )
 }
