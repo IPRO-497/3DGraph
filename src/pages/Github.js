7f2d2ae402a401ad8eddba1e2afa2cc75bc42972
Tayomide <tibrahim@hawk.iit.edu>
2023-02-04 19:36:55 -0600

    Get and convert data from netlify function

--- src/pages/Github.js
+++ src/pages/Github.js
@@ -1,15 +1,22 @@
 import { OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import { YearWeekDayGroup } from '../components/YearWeekDayGroup'
+import { useEffect, useState } from 'react'
 
 export const Github = () => {
-  const data = require("../data/GithubDummy").data
-  const converter = require("../converter/Github")
-  const convertedData = converter.convert(data)
+  const [convertedData, setConvertedData] = useState()
+  useEffect(() => {
+    const converter = require("../converter/Github")
+    fetch("/.netlify/functions/github")
+    .then(response => response.json())
+    .then(response => {
+      setConvertedData(converter.convert(response))
+    })
+  }, [])
   return (
     <Canvas>
       <OrbitControls />
-      <YearWeekDayGroup convertedData={convertedData} username="tayomide" year="2022" website="github" />
+      {convertedData && <YearWeekDayGroup convertedData={convertedData} username="tayomide" year="2022" website="github" />}
     </Canvas>
   )
 }
--- src/pages/LeetCode.js
+++ src/pages/LeetCode.js
@@ -2,15 +2,22 @@
 import { OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import { YearWeekDayGroup } from '../components/YearWeekDayGroup'
+import { useEffect, useState } from "react"
 
 export const LeetCode = () => {
-  const leetCodeData = require("../data/LeetCodeDummy").data
-  const converter = require("../converter/LeetCode")
-  const convertedData = converter.convert(leetCodeData)
+  const [convertedData, setConvertedData] = useState()
+  useEffect(() => {
+    const converter = require("../converter/LeetCode")
+    fetch("/.netlify/functions/leetcode")
+    .then(response => response.json())
+    .then(response => {
+      setConvertedData(converter.convert(response))
+    })
+  }, [])
   return (
     <Canvas>
       <OrbitControls />
-      <YearWeekDayGroup convertedData={convertedData} username="tayomide" year="2022" website="leetcode" />
+      {convertedData && <YearWeekDayGroup convertedData={convertedData} username="tayomide" year="2022" website="leetcode" />}
     </Canvas>
   )
 }
