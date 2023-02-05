eed66d3690b09f3d1cdcfa3bd4d58fdce283c7dd
Tayomide <tibrahim@hawk.iit.edu>
2023-02-04 23:37:24 -0600

    Send parameters to functions and request

--- src/pages/LeetCode.js
+++ src/pages/LeetCode.js
@@ -3,21 +3,23 @@ import { OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import { YearWeekDayGroup } from '../components/YearWeekDayGroup'
 import { useEffect, useState } from "react"
+import { useParams } from 'react-router-dom'
 
 export const LeetCode = () => {
   const [convertedData, setConvertedData] = useState()
+  const params = useParams()
   useEffect(() => {
     const converter = require("../converter/LeetCode")
-    fetch("/.netlify/functions/leetcode")
+    fetch(`/.netlify/functions/leetcode?name=${params.name}&year=${params.year}`)
     .then(response => response.json())
     .then(response => {
-      setConvertedData(converter.convert(response))
+      setConvertedData(converter.convert(response, params.year))
     })
   }, [params])
   return (
     <Canvas>
       <OrbitControls />
-      {convertedData && <YearWeekDayGroup convertedData={convertedData} username="tayomide" year="2022" website="leetcode" />}
+      {convertedData && <YearWeekDayGroup convertedData={convertedData} username={params.name} year={params.year} website="leetcode" />}
     </Canvas>
   )
 }
