ad0a81d866d9c1b1b8d8abdc16efd1537a79d667
Tayomide <tibrahim@hawk.iit.edu>
2023-02-07 00:19:22 -0600

    Convert data from ML model

--- src/pages/TensorFlow.js
+++ src/pages/TensorFlow.js
@@ -1,32 +1,52 @@
 import styled from 'styled-components';
 import * as handDetection from '@tensorflow-models/hand-pose-detection';
-import { Hands, HAND_CONNECTIONS, VERSION } from "@mediapipe/hands";
-import { Camera } from '@mediapipe/camera_utils';
+import { VERSION } from "@mediapipe/hands";
 // import * as cocoSsd from "@tensorflow-models/coco-ssd"
 import "@tensorflow/tfjs-backend-webgl"
 import "@tensorflow/tfjs-backend-cpu"
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useContext } from 'react';
+import { HandContext } from '../hooks/HandContext'
 
 export const TensorFlow = () => {
   const video = useRef()
   const canvas = useRef()
-  const count = useRef(1)
+  const count = useRef(2)
+  const streamRef = useRef()
+  const {
+    leftConstant,
+    rightConstant,
+    leftCurrent,
+    rightCurrent
+  } = useContext(HandContext)
 
-  const onResults = (results) => {
-    console.log(results)
+  const distanceIsLess = (nodes, group) => {
+    const distance = []
+    for(let i = 1; i < 5; i++){
+      const node = group * 4 + i
+      distance.push((nodes[node].x - nodes[0].x)**2 + (nodes[node].y - nodes[0].y)**2)
+    }
+    for(let i = 0; i < 3; i++){
+      if(distance[i] > distance[i+1])return false
+    }
+    return true
   }
 
-  // const hands = new Hands({locateFile: (file) => {
-  //   return `https://cdn.jsdelivr.net/npm/@mediapipe/hands@${VERSION}`;
-  // }})
-
-  // hands.setOptions({
-    //   maxNumHands: 2,
-    //   modelComplexity: 1,
-    //   minDetectionConfidence: 0.5,
-    //   minTrackingConfidence: 0.5
-    // })
-    // hands.onResults(onResults)
+  const isOpen = (data) => {
+    const hands = {}
+    for(const obj of data){
+      let hand = true
+      for(let i = 0; i < 5; i++){
+        if(!distanceIsLess(obj.keypoints, i)){
+          hand = false
+          break;
+        }
+      }
+      hands[obj.handedness] = hand
+    }
+    if(!hands["Left"])hands["Left"] = false
+    if(!hands["Right"])hands["Right"] = false
+    return hands
+  }
 
   const startWebcam = async () => {
     const detector = await handDetection.createDetector("MediaPipeHands",{
@@ -40,136 +60,111 @@ export const TensorFlow = () => {
       detector.estimateHands(video.current, {
         flipHorizontal: true
       }).then(hands => {
-        console.log(hands)
+        const controls = isOpen(hands)
+        if(!controls.Left){
+          leftCurrent.current = []
+          leftConstant.current = []
+        }
+        else{
+          const obj = hands.find(obj => obj.handedness === "Left")
+          if(!leftConstant.current.length)leftConstant.current = [
+            obj.keypoints3D[0].x,
+            obj.keypoints3D[0].y,
+            obj.keypoints3D[0].z
+          ]
+          leftCurrent.current = [
+            obj.keypoints3D[0].x,
+            obj.keypoints3D[0].y,
+            obj.keypoints3D[0].z
+          ]
+        }
+        if(!controls.Right){
+          rightCurrent.current = []
+          rightConstant.current = []
+        }
+        else{
+          const obj = hands.find(obj => obj.handedness === "Right")
+          if(!rightConstant.current.length)rightConstant.current = [
+            obj.keypoints3D[0].x,
+            obj.keypoints3D[0].y,
+            obj.keypoints3D[0].z
+          ]
+          rightCurrent.current = [
+            obj.keypoints3D[0].x,
+            obj.keypoints3D[0].y,
+            obj.keypoints3D[0].z
+          ]
+        }
+        console.log({rightConstant, rightCurrent, leftConstant, leftCurrent})
       })
-      window.requestAnimationFrame(predictWebcam)
+      .catch(err => console.log(err))
+      if(video?.current?.srcObject)window.requestAnimationFrame(predictWebcam)
     }
     predictWebcam()
   }
 
-  const startVideo = () => {
-    console.log("working")
-    video.current.srcObject = null
-    navigator.mediaDevices.getUserMedia({video: true})
-    .then(stream => {
-      video.current.srcObject = stream
+  const stopVideo = () => {
+    const stream = video?.current?.captureStream() || streamRef.current;
+    const tracks = stream.getTracks();
+    for (var i = 0; i < tracks.length; i++) {
+      tracks[i].stop();
+      stream.removeTrack(tracks[i])
+    }
+    if(!video?.current?.srcObject)return
+    video.current.srcObject = null;
+  }
+  
+
+  const startVideo = async () => {
+    if(!!(video.current.srcObject))return
+    try{
+      if(!!!(navigator.mediaDevices.getUserMedia))throw new Error('media device is not supported.');
+      const stream = await navigator.mediaDevices.getUserMedia({video: true})
+      streamRef.current = stream
+      console.log(stream)
+      if(!video.current.srcObject)video.current.srcObject = stream
       video.current.addEventListener("loadeddata", startWebcam)
-    },
-    err => {
+      window.stream = stream
+    } catch(err){
+      stopVideo()
       if(count.current){
         startVideo();
         count.current--
       }else console.log(err)
     }
-    )
   }
-  
+
   useEffect(() => {
     startVideo()
+    return () => {
+      count.current = 2
+      stopVideo()
+    }
   }, [])
-
-
-
-
-
-  // const video = useRef()
-  // const liveView = useRef()
-  // const enableWebcamButton = useRef()
-  // const count = useRef(1)
-  // const model = useRef(false)
-  // const children = useRef([])
-
-  // const getUserMediaSupported = () => {
-  //   return !!(navigator.mediaDevices &&
-  //     navigator.mediaDevices.getUserMedia);
-  // }
-
-  // useEffect(() => {
-  //   cocoSsd.load()
-  //   .then((loadModel) => {
-  //     model.current = loadModel
-  //     enableWebcamButton.current.classList.remove('removed');
-  //   })
-  // }, [])
-
-  // const predictWebcam = (e) => {
-  //   model.current.detect(video.current)
-  //   .then((predictions) => {
-  //     for(let i = 0; i < children.current.length; i++){
-  //       liveView.current.removeChild(children.current[i])
-  //     }
-  //     children.current.splice(0)
-
-  //     for(let n = 0; n < predictions.length; n++){
-  //       if(predictions[n].score > 0.66){
-  //         const p = document.createElement('p');
-  //         p.innerText = predictions[n].class  + ' - with ' 
-  //             + Math.round(parseFloat(predictions[n].score) * 100) 
-  //             + '% confidence.';
-  //         p.style = 'margin-left: ' + predictions[n].bbox[0] + 'px; margin-top: '
-  //             + (predictions[n].bbox[1] - 10) + 'px; width: ' 
-  //             + (predictions[n].bbox[2] - 10) + 'px; top: 0; left: 0;';
-
-  //         const highlighter = document.createElement('div');
-  //         highlighter.setAttribute('class', 'highlighter');
-  //         highlighter.style = 'left: ' + predictions[n].bbox[0] + 'px; top: '
-  //             + predictions[n].bbox[1] + 'px; width: ' 
-  //             + predictions[n].bbox[2] + 'px; height: '
-  //             + predictions[n].bbox[3] + 'px;';
-
-  //         liveView.current.appendChild(highlighter);
-  //         liveView.current.appendChild(p);
-  //         children.current.push(highlighter);
-  //         children.current.push(p);
-  //       }
-  //     }
-  //     window.requestAnimationFrame(predictWebcam)
-  //   })
-  // }
-
-  // const constraints = {
-  //   video: true
-  // }
-
-  // const startVideo = (e) => {
-  //   if(!model.current){//model
-  //     return
-  //   }
-  //   if(!e.target.disabled) e.target.disabled = true
-  //   video.current.srcObject = null
-  //   navigator.mediaDevices.getUserMedia(constraints)
-  //   .then(stream => {
-  //     video.current.srcObject = stream
-  //     video.current.addEventListener("loadeddata", predictWebcam)
-  //   },
-  //   err => {
-  //     if(count.current){
-  //       startVideo(e);
-  //       count.current--
-  //     }else console.log(err)
-  //   }
-  //   )
-  // }
-
-
 
   return (
     <Container>
-      {/* <div id="liveView" className="camView" ref={liveView}>
-        <button id="webcamButton" className="removed" ref={enableWebcamButton} onClick={getUserMediaSupported() ? startVideo : (e) => console.log("nope")}>Enable Webcam</button>
-        <video id="webcam" autoPlay muted width="640" height="480" ref={video} src=""></video>
-      </div> */}
       <div className="container">
-        <video className="input_video" ref={video} autoPlay muted width="640" height="480"></video>
-        <canvas className="output_canvas" width="1280px" height="720px" ref={canvas}></canvas>
+        <button onClick={() => startVideo()}>Start Logic</button>
+        <button onClick={() => stopVideo()}>End Logic</button>
+        <video className="input_video" ref={video} onLoad={e => console.log(e)} autoPlay >
+          <source src="/small.mp4" type='video/mp4' />
+        </video>
+        <canvas className="output_canvas" width="100vw" height="100vh" ref={canvas}></canvas>
       </div>
     </Container>
   )
 }
 
 const Container = styled.div`
+  position:fixed;
+  top:-9999px;
+  opacity:0;
+  visibility: hidden;
   video {
     display: block;
+    width: 100vw;
+    height: 100vh;
   }
   button.removed{
     user-select: none;
