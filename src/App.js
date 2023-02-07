38041da3134090f5edf0c5efbc94675340822deb
Tayomide <tibrahim@hawk.iit.edu>
2023-02-07 00:20:16 -0600

    Add context hook and define variables

--- src/App.js
+++ src/App.js
@@ -3,29 +3,30 @@ import { Block } from './pages/Block';
 import { Github } from './pages/Github';
 import { Home } from './pages/Home';
 import { LeetCode } from './pages/LeetCode';
+import { TensorFlow } from './pages/TensorFlow';
+import { HandContext } from './hooks/HandContext';
+import { useRef } from 'react';
 
 function App() {
-  // fetch("http://localhost:5000/leetcode?username=tayomide", {
-  //   method: "GET"
-  // })
-  // .then(response => response.json())
-  // .then(response => console.log(response))
-
-  // fetch("http://localhost:5000/github?user=tayomide&year=2022", {
-  //   method: "GET"
-  // })
-  // .then(response => response.json())
-  // .then(response => console.log(response))
-
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/block" element={<Block />} />
-        <Route exact path="/leetcode/:name/:year" element={<LeetCode />} />
-        <Route exact path="/github/:name/:year" element={<Github />} />
-      </Routes>
-    </Router>
+    <HandContext.Provider value={{
+      leftConstant: useRef([]),
+      rightConstant: useRef([]),
+      leftCurrent: useRef([]),
+      rightCurrent: useRef([]),
+      positionConstant: useRef([]),
+      rotationConstant: useRef([])
+    }}>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/block" element={<Block />} />
+          <Route exact path="/tensorflow" element={<TensorFlow />} />
+          <Route exact path="/leetcode/:name/:year" element={<LeetCode />} />
+          <Route exact path="/github/:name/:year" element={<Github />} />
+        </Routes>
+      </Router>
+    </HandContext.Provider>
   );
 }
 
