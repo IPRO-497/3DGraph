7e3ea1043b0ed18087195da2e67b6cb87daeab0c
Tayomide <tibrahim@hawk.iit.edu>
2023-02-07 00:20:02 -0600

    Modify Block Component to test new controls

--- src/components/Block.js
+++ src/components/Block.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import { useState, useContext } from 'react'
 import { useThree } from '@react-three/fiber'
 import { STLExporter } from "three/examples/jsm/exporters/STLExporter"
+import styled from 'styled-components'
+import { Html } from '@react-three/drei'
+import { HandContext } from '../hooks/HandContext'
 
-export const BlockMesh = () => {
-  const { scene } = useThree()
+export const BlockMesh = ({setTensor, tensor}) => {
+  const {positionConstant, rotationConstant} = useContext(HandContext)
+  const [popup, setPopup] = useState(false)
+  const { scene, camera } = useThree()
   const link = document.createElement('a')
   const save = (blob, filename) => {
     link.href = URL.createObjectURL(blob)
@@ -21,10 +26,47 @@ export const BlockMesh = () => {
     //   scene
     // )
   }
+
+  const toggleControls = () => {
+    setTensor(!tensor)
+    positionConstant.current = [
+      camera.position.x,
+      camera.position.y,
+      camera.position.z,
+    ]
+    rotationConstant.current = [
+      camera.rotation.x,
+      camera.rotation.y,
+      camera.rotation.z
+    ]
+  }
+
+
   return (
-    <mesh onClick={download}>
-      <boxGeometry />
-      <meshBasicMaterial color="red"/>
-    </mesh>
+    <group>
+      <mesh onClick={() => setPopup(!popup)}>
+        <boxGeometry />
+        <meshBasicMaterial color="red"/>
+      </mesh>
+      {
+        popup && 
+        <PopUp position={[0, 0, 0]}>
+          <div>
+            <button onClick={download}>Download</button>
+            <button onClick={() => toggleControls()}>Toggle Tensor</button>
+          </div>
+        </PopUp>
+      }
+    </group>
   )
 }
+
+const PopUp = styled(Html)`
+  background-color: red;
+  height: 15em;
+  width: 20em;
+  button{
+    background-color: blue;
+    padding: 1px;
+  }
+`
