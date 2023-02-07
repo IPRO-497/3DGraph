2a752517b5f12b4a0505df2dc13a9fe184bce403
Tayomide <tibrahim@hawk.iit.edu>
2023-02-07 00:20:45 -0600

    Add variable to toggle new controls

--- src/pages/Block.js
+++ src/pages/Block.js
@@ -1,13 +1,21 @@
 import { OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
-import React from 'react'
+import { useState } from 'react'
 import { BlockMesh } from '../components/Block'
+import { TensorFlow } from './TensorFlow'
 
 export const Block = () => {
+  const [tensor, setTensor] = useState(false)
   return (
+    <>
+    {
+      tensor && 
+      <TensorFlow />
+    }
     <Canvas>
       <OrbitControls />
-      <BlockMesh />
+      <BlockMesh setTensor={setTensor} tensor={tensor}/>
     </Canvas>
+    </>
   )
 }
