dee9d3480cd9177b45b95c735f97e520e23c771f
Tayomide <tibrahim@hawk.iit.edu>
2023-03-26 20:35:38 -0500

    Redesign home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,17 @@
 import styled from "styled-components"
+import { Form } from "../components/Form"
 
 export const Home = () => {
   return (
+    <Container>
+      <Form />
     </Container>
   )
 }
 
 const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`
