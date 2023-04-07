84621523eeb079ea0f0a0bbfbf7fba0f64d296e8
Tayomide <tibrahim@hawk.iit.edu>
2023-04-07 01:40:07 -0500

    Create netlify function for stripe
    This function handles redirecting logic for stripe payment

---
+++ functions/stripe/stripe.js
@@ -0,0 +1,32 @@
+const stripe = require('stripe')(process.env.REACT_APP_STRIPE_TEST_SECRET)
+
+const handler = async (event) => {
+  const items = JSON.parse(event.body)
+  const lineItems = []
+  for(const item of Object.values(items)){
+    if(item.download)lineItems.push({
+      // price: "price_1MrsfJI1aNradUj24XS9HdlY",
+      price: "price_1Mrs1tI1aNradUj20Eei7uzm",
+      quantity: 1
+    })
+    if(item.ship)lineItems.push({
+      // price: "price_1MrsfJI1aNradUj24XS9HdlY",
+      price: "price_1Mrs1tI1aNradUj20Eei7uzm",
+      quantity: parseInt(item.quantity)
+    })
+  }
+  const session = await stripe.checkout.sessions.create({
+    line_items: lineItems,
+    mode: "payment",
+    success_url: "http://localhost:8888/success",
+    cancel_url: "http://localhost:8888/cancel"
+  });
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      url: session.url
+    })
+  }
+}
+
+module.exports = { handler }
