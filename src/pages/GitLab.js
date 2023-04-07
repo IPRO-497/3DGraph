71e691dcec791c93c64464a579c1d788e9504bb1
Tayomide <tibrahim@hawk.iit.edu>
2023-04-07 02:05:03 -0500

    Create page for Gitlab contributions

---
+++ src/pages/GitLab.js
@@ -0,0 +1,49 @@
+
+import { OrbitControls } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import { YearWeekDayGroup } from '../components/YearWeekDayGroup'
+import { useContext, useEffect, useState } from "react"
+import { MotionCamera } from '../components/MotionCamera'
+import { TensorFlow } from '../components/TensorFlow'
+import { MenuContext } from '../hooks/MenuHook'
+import { ButtonStyle } from './ButtonStyle'
+import { useParams } from 'react-router-dom'
+
+export const GitLab = ({name, year}) => {
+  // Names: feistel, dnsmichi
+  const [convertedData, setConvertedData] = useState()
+  const params = useParams()
+  if(typeof name !== "string")name = params.name
+  if(typeof year !== "string")year = (new Date().getMonth()).toString() + "/" +
+  (new Date().getFullYear()).toString().slice(2,4)
+  // Tensor Logic - Change to Context
+  const [tensor, setTensor] = useState(false)
+  const {show} = useContext(MenuContext)
+
+  useEffect(() => {
+    const converter = require("../converter/GitLab")
+    fetch(`/.netlify/functions/gitlab?name=${name}`)
+    .then(response => response.json())
+    .then(response => {
+      setConvertedData(converter.convert(response))
+    })
+  }, [name])
+
+  return (
+    <>
+      {
+        tensor && 
+        <TensorFlow />
+      }
+      {
+        show &&
+        <ButtonStyle />
+      }
+      <Canvas className='canvas'>
+        {tensor ? <MotionCamera />: <OrbitControls />}
+        <OrbitControls />
+        {convertedData && <YearWeekDayGroup convertedData={convertedData} username={name} year={year} website="gitlab" setTensor={setTensor} tensor={tensor}/>}
+      </Canvas>
+    </>
+  )
+}
