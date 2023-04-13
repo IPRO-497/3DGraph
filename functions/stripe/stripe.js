c3d844f91e7a1d561903adcbe8cabddc6901a8b6
Tayomide <tibrahim@hawk.iit.edu>
2023-04-12 20:43:45 -0500

    Change logic on sorting products

--- functions/stripe/stripe.js
+++ functions/stripe/stripe.js
@@ -4,16 +4,39 @@ const handler = async (event) => {
   const items = JSON.parse(event.body)
   const lineItems = []
   for(const item of Object.values(items)){
-    if(item.download)lineItems.push({
-      // price: "price_1MrsfJI1aNradUj24XS9HdlY",
-      price: "price_1Mrs1tI1aNradUj20Eei7uzm",
-      quantity: 1
-    })
-    if(item.ship)lineItems.push({
-      // price: "price_1MrsfJI1aNradUj24XS9HdlY",
-      price: "price_1Mrs1tI1aNradUj20Eei7uzm",
-      quantity: parseInt(item.quantity)
-    })
+    console.log(process.env.REACT_APP_GITHUBCD)
+    if(item.download){
+      if(item.model.toLowerCase() === "contribution"){
+        if(item.website.toLowerCase() === "github")lineItems.push({
+          price: process.env.REACT_APP_GITHUBCD,
+          quantity: 1
+        })
+        if(item.website.toLowerCase() === "gitlab")lineItems.push({
+          price: process.env.REACT_APP_GITLABCD,
+          quantity: 1
+        })
+        if(item.website.toLowerCase() === "leetcode")lineItems.push({
+          price: process.env.REACT_APP_LEETCODECD,
+          quantity: 1
+        })
+      }
+    }
+    else if(item.ship){
+      if(item.model.toLowerCase() === "contribution"){
+        if(item.website.toLowerCase() === "github")lineItems.push({
+          price: process.env.REACT_APP_GITHUBCS,
+          quantity: item.quantity
+        })
+        if(item.website.toLowerCase() === "gitlab")lineItems.push({
+          price: process.env.REACT_APP_GITLABCS,
+          quantity: item.quantity
+        })
+        if(item.website.toLowerCase() === "leetcode")lineItems.push({
+          price: process.env.REACT_APP_LEETCODECS,
+          quantity: item.quantity
+        })
+      }
+    }
   }
   const session = await stripe.checkout.sessions.create({
     line_items: lineItems,
