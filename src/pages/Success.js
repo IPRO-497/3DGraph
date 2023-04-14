292ac171e6b35443fe8fe45ceb2e2d4bea8b1784
Tayomide <tibrahim@hawk.iit.edu>
2023-04-13 19:09:09 -0500

    Create success component

---
+++ src/pages/Success.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import cryptoJs from 'crypto-js'
+
+export const Success = () => {
+  return (
+    <div>Success</div>
+  )
+}
