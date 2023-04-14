c1ff7ebe3598f086e1280ab35d1d37fe3f332176
Tayomide <tibrahim@hawk.iit.edu>
2023-04-13 19:10:36 -0500

    Create verification logic
    If the hashed id is the same as hashed guid saved in local storage, return true

--- src/pages/Success.js
+++ src/pages/Success.js
@@ -3,6 +3,11 @@ import { useParams } from 'react-router-dom'
 import cryptoJs from 'crypto-js'
 
 export const Success = () => {
+  const params = useParams()
+  console.log(cryptoJs.SHA256(params.id).toString() === localStorage["uuid"])
+  // Once you empty the cart set the local storage uuid to nothing
+  // Keep coming soon for the ship button and quantity
+  // Add fill form functionality
   return (
     <div>Success</div>
   )
