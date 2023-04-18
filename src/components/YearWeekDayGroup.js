deacd283dcef7866d9114e473d1511f54178931e
Tayomide <tibrahim@hawk.iit.edu>
2023-04-18 18:40:28 -0500

    Remove redundant code

--- src/components/YearWeekDayGroup.js
+++ src/components/YearWeekDayGroup.js
@@ -96,12 +96,6 @@ export const YearWeekDayGroup = ({convertedData, username, year, website, setTen
     set({username: username, year: year})
   }, [set, username, year])
 
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     download()
-  //   }, 1000);
-  // }, [download])
-
   return (
     <>
     <Environment
