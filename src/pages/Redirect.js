0a7aa2bb18dd686692abaf81a6093905a44fa9f9
Tayomide <tibrahim@hawk.iit.edu>
2023-04-19 16:44:38 -0500

    Delete Redirect component

--- src/pages/Redirect.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Navigate, useParams } from 'react-router-dom'
-
-export const NavigateComponent = ({to}) => {
-  const { id } = useParams()
-  return (
-    <Navigate to={to} state={{ service: id }} />
-  )
-}
