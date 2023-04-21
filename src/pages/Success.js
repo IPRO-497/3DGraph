b68482c654ebef9634b931cb3bd30ac74c30b648
Tayomide <tibrahim@hawk.iit.edu>
2023-04-20 23:53:45 -0500

    Define download logic

--- src/pages/Success.js
+++ src/pages/Success.js
@@ -10,7 +10,40 @@ import { CartItem } from '../components/CartItem'
 
 export const Success = () => {
   const params = useParams()
-  console.log(cryptoJs.SHA256(params.id).toString() === localStorage["uuid"])
+  const {setCartItems, cartItems} = useContext(MenuContext)
+  const [taskComplete, setTaskComplete] = useState(false)
+  const [listItems, setListItems] = useState({})
+  const count = useRef(0)
+
+  useEffect(() => {
+    if(Object.keys(cartItems).length){
+      if(taskComplete === true){
+        setCartItems(prevCartItems => {
+          const tempCartItems = {...prevCartItems}
+          delete tempCartItems[Object.keys(prevCartItems)[0]]
+          return tempCartItems
+        })
+        setListItems(prevCartItems => {
+            const tempCartItems = {...prevCartItems}
+            tempCartItems[Object.keys(tempCartItems)[count.current]] &&
+            (tempCartItems[Object.keys(tempCartItems)[count.current]].downloaded = "Downloaded")
+            count.current += 1
+            return tempCartItems
+          }
+        )
+      }
+      setTaskComplete(false)
+    }
+    if(!Object.keys(cartItems).length){
+      localStorage["uuid"] = ""
+    }
+  }, [taskComplete, cartItems, setCartItems, setListItems])
+
+  useEffect(() => {
+    setListItems(JSON.parse(localStorage["cartItems"]))
+  }, [setListItems])
+
+  // console.log(cryptoJs.SHA256(params.id).toString() === localStorage["uuid"])
   // Once you empty the cart set the local storage uuid to nothing
   // Keep coming soon for the ship button and quantity
   // Add fill form functionality
